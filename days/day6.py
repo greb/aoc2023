c4f570f83af2ee6457790ad10bcc881b71c69981
greb <greb>
2023-12-06 09:52:36 +0100

    Optimize day 6
    * Replace brute force calculations with math

--- days/day6.py
+++ days/day6.py
@@ -1,30 +1,23 @@
 import re
 import math
 
+
+def count_wins(time, dist):
+    det = (time*time/4.0) - dist
+    lower = time/2.0 - math.sqrt(det)
+    upper = time/2.0 + math.sqrt(det)
+    return math.ceil(upper) - math.floor(lower) - 1
+
+
 def part1(inp):
     times, dists = inp.splitlines()
     times = [int(n) for n in re.findall('\d+', times)]
     dists = [int(n) for n in re.findall('\d+', dists)]
-
-    win_counts = []
-    for time, old_dist in zip(times, dists):
-        cnt = 0
-        for hold in range(time+1):
-            dist = hold * (time-hold)
-            if dist > old_dist:
-                cnt += 1
-        win_counts.append(cnt)
-    return math.prod(win_counts)
+    return math.prod(count_wins(*elem) for elem in zip(times, dists))
 
 
 def part2(inp):
     time, dist = inp.splitlines()
     time = int(''.join(re.findall('\d', time)))
-    old_dist = int(''.join(re.findall('\d', dist)))
-
-    cnt = 0
-    for hold in range(time+1):
-        dist = hold * (time-hold)
-        if dist > old_dist:
-            cnt += 1
-    return cnt
+    dist = int(''.join(re.findall('\d', dist)))
+    return count_wins(time, dist)
