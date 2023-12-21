89d0ad2f1585c282853fd9c5ec2fded295157fb0
greb <greb>
2023-12-21 16:23:14 +0100

    Solve day 21, part 2
    * Honestly, I had to look up the solution on reddit. Apparently, like
      day 20, the input was designed in such a way that it could be
      exploited for a realistic solution time.

--- days/day21.py
+++ days/day21.py
@@ -20,20 +20,31 @@ def moves(pos):
         yield tuple(p+d for p,d in zip(pos, dir))
 
 
-def in_bounds(pos, bounds):
-    return all(0 < p <= b for p,b in zip(pos, bounds))
+def wrap(pos, bounds):
+    return tuple(p%b for p,b in zip(pos, bounds))
 
 
 def step(positions, rocks, bounds):
     new_positions = set()
     for pos in positions:
         for move in moves(pos):
-            if move in rocks:
-                continue
-            if not in_bounds(move, bounds):
+            if wrap(move, bounds) in rocks:
                 continue
             new_positions.add(move)
     return new_positions
+
+
+def display(positions, rocks, bounds):
+    for y in range(bounds[0]):
+        line = []
+        for x in range(bounds[1]):
+            if (x,y) in rocks:
+                line.append('#')
+            elif (x,y) in positions:
+                line.append('O')
+            else:
+                line.append('.')
+        print(''.join(line))
 
 
 def part1(garden):
@@ -41,3 +52,28 @@ def part1(garden):
     for _ in range(64):
         positions = step(positions, rocks, bounds)
     return len(positions)
+
+
+def part2(garden):
+    positions, rocks, bounds = garden
+    assert bounds[0] == bounds[1]
+    size = bounds[0]
+    goal = 26501365
+
+    steps = 0
+    ys = []
+    while True:
+        positions = step(positions, rocks, bounds)
+        steps += 1
+        if steps % size == goal % size:
+            ys.append(len(positions))
+            if len(ys) == 3:
+                break
+
+    # use data points for quadratic interpolation
+    x = goal // size
+    a, b, c = ys
+    return a + (b-a)*x + ((c-b)-(b-a))*(x*(x-1)//2)
+
+
+
